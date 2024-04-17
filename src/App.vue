8ae1721272168d7d3347b57ece4e865266d8ab99
itsjackie00 <jackiedanso33@gmail.com>
2024-04-17 11:18:54 +0200

    created app vue, imported axios and store. created a function to get movies and series with anxios call . then created a function to see the array in the console

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,36 @@
 </template>
 
 <script>
+import axios from 'axios';
+import {store} from './store';
+
+
   export default {
-    name: 'App'
+    name: 'App',
+    data () {
+      return {
+        store
+      }
+    },
+    methods: {
+      getMovies(){
+        axios.get(this.store.apiUrl + this.store.endPoint.movie, this.store.options).then 
+        ((res) => {
+          console.log(res.data.results);
+        })
+      },
+      getTvSeries(){
+        axios.get(this.store.apiUrl + this.store.endPoint.tv, this.store.options).then 
+        ((res) => {
+          console.log(res.data.results);
+        })
+      }
+
+      },
+    created(){
+      this.getMovies();
+      this.getTvSeries();
+    }
   }
 </script>
 
