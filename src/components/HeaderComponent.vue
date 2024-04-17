80eb7114def947a622e422370e4b3172eb33331a
itsjackie00 <jackiedanso33@gmail.com>
2024-04-17 13:14:23 +0200

    fix

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,11 @@
 <template>
     <header>
         <div class="container">
-            <div class="row">
-                <div class="col-9">
-                    <h1 class="text-danger text-uppercase p-3 ">Boolflix</h1>
+            <div class="row p-3">
+                <div class="col-8 col-sm-6 col-md-7 col-lg-8 col-xl-9 d-flex align-items-center">
+                    <h1 class="text-danger text-uppercase ">Boolflix</h1>
                 </div>
-                <div class="col-3 p-4">
+                <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3 d-flex align-items-center">
                     <input type="text" placeholder="Cerca">
                     <button>
                         <i class="fa-solid fa-magnifying-glass"></i>
