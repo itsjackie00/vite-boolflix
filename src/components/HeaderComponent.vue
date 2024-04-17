7527ae41f342f96925525f8713b0d2c936cb085a
itsjackie00 <jackiedanso33@gmail.com>
2024-04-17 12:50:04 +0200

    added h1 and input type text

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,1 +1,32 @@
-<!-- bbb1041a54aa10011524a120962b32a1 -->
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-9">
+                    <h1 class="text-danger text-uppercase p-3 ">Boolflix</h1>
+                </div>
+                <div class="col-3 p-4">
+                    <input type="text" placeholder="Cerca">
+                    <button>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    width: 100%;
+    height: 100px;
+    background-color: black;
+}
+</style>
