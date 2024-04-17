1f125066a132b6e753ecacc2982aa6f96439d000
itsjackie00 <jackiedanso33@gmail.com>
2024-04-17 11:19:29 +0200

    imported reactive, created my store with two arrays

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import {reactive} from 'vue';
+
+export const store = reactive ({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    endPoint: {
+        movie:'search/movie',
+        tv: 'search/tv'
+    },
+    options: {
+        params: {
+            api_key: 'bbb1041a54aa10011524a120962b32a1',
+            query: 'a'
+        }
+    },
+    imageUrl: 'https://image.tmdb.org/t/p/original'
+});
