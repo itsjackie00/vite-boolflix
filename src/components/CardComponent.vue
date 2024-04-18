a0bb9e08e3b9842407beb6f8986de1db6b816306
itsjackie00 <jackiedanso33@gmail.com>
2024-04-18 09:05:26 +0200

    added content

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card mt-3">
+        <img :src="image" class="card-img-top" :alt="title">
+        <div class="card-body">
+            <h5 class="card-title text-white ">{{ title }}</h5>
+            <p class="card-text">
+                <span>{{ flag }}</span>
+                <br>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CardComponent',
+        props: ['id', 'title', 'image', 'flag']
+    }
+</script>
+
+<style lang="scss" scoped>
+.card{
+    text-align: center;
+    color: black;
+    background-color: #D48F38;
+    height: 520px;
+    border-radius: 0;
+}
+
+</style>
