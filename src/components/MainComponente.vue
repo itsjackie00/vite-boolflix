a72dfb7c4b3bbbeee23784900f770de4c1cb0b18
itsjackie00 <jackiedanso33@gmail.com>
2024-04-18 09:03:33 +0200

    added main bg

---
+++ src/components/MainComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+        main 
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponente',
+    }
+</script>
+
+<style lang="scss" scoped>
+main{
+    background-color: #141414;
+    color : white;
+}
+
+</style>
