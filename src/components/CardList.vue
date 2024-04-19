5a8552a2ce31391b7f037532e014a405b7b9e0bc
itsjackie00 <jackiedanso33@gmail.com>
2024-04-19 19:37:17 +0200

    fix cards. added justify content start

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -9,9 +9,21 @@
             </div>
         </div>
 
-        <div class=" d-flex flex-column">
+        <!-- <div class=" d-flex flex-column">
             <h4 class="text-white p-3 "> Top 10 in Italy Today </h4>
             <div id="card-container-top-10" ref="cardContainer">
+                <div v-for="(trending, index) in store.trending.slice(0, 10)" :key="index">
+                    <div class="d-flex">
+                        <h2 class="big-number">{{ index + 1 }} </h2>
+                        <CardComponent :id="trending.id" :image="trending.image" />
+                    </div>
+                </div>
+            </div> 
+        </div>-->
+
+        <div class="d-flex flex-column">
+            <h4 class="text-white p-3 "> Top 10 in Italy Today </h4>
+            <div id="card-container-top-10">
                 <div v-for="(trending, index) in store.trending.slice(0, 10)" :key="index">
                     <div class="d-flex">
                         <h2 class="big-number">{{ index + 1 }} </h2>
@@ -23,7 +35,7 @@
 
         <div class="d-flex flex-column mt-5 ">
             <h4 class="text-white p-3 "> Tv Series </h4>
-            <div class="d-flex" id="card-container-top-10" ref="cardContainer">
+            <div class="d-flex" id="card-container-top-10">
                 <div v-for="(tv, index) in store.tv" :key="index">
                     <CardComponent :id="tv.id" :title="tv.title" :image="tv.image" :flag="tv.flag" />
                 </div>
@@ -34,7 +46,7 @@
             <h4 class="text-white p-3 "> Most Popular in Italy </h4>
             <div class="d-flex" id="card-container-top-10" ref="cardContainer">
                 <div v-for="(popular, index) in store.popular" :key="index">
-                    <CardComponent :id="popular.id" :image="popular.image"/>
+                    <CardComponent :id="popular.id" :image="popular.image" />
                 </div>
             </div>
         </div>
@@ -57,7 +69,7 @@ export default {
         }
     },
     mounted() {
-        this.$refs.cardContainer.scrollLeft = 0;
+        this.$refs.cardContainer.scrollLeft = this.$refs.cardContainer.scrollWidth;
     }
 }
 </script>
@@ -67,8 +79,8 @@ export default {
     display: flex;
     overflow: auto;
     white-space: nowrap;
-    justify-content: center;
-    align-items: center;
+    justify-content: start;
+    align-items: start;
     gap: 4rem;
     margin-bottom: 1rem;
     margin-top: 1rem;
