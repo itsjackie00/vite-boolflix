66ba2dfe752f5a59e7a6477cc5fa7ef02a02af03
itsjackie00 <jackiedanso33@gmail.com>
2024-04-19 18:15:13 +0200

    added padding top

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="fs-6" id="jumbotron">
+        Jumbotron
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Jumbotron'
+    }
+</script>
+
+<style lang="scss" scoped>
+#jumbotron {
+    padding-top: 200px;
+}
+</style>
