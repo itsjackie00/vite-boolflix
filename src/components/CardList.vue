6672700fa6df1357c8199fe8ac55cda881fe6e93
itsjackie00 <jackiedanso33@gmail.com>
2024-04-19 16:06:59 +0200

    added numbers for top 10

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,9 +1,23 @@
 <template>
-    <div class="container">
+    <div class="">
         <h4 class="text-white p-3 "> Movies </h4>
         <div class="row" id="card-container">
             <div class="col-12 col-md-6 col-lg-3 " v-for="(movie, index) in store.movies" :key="index">
                 <CardComponent :id="movie.id" :title="movie.title" :image="movie.image" :flag="movie.flag" />
+            </div>
+        </div>
+
+        <div class=" d-flex flex-wrap">
+            <h4 class="text-white p-3 "> Top 10 </h4>
+            <div id="card-container-top-10">
+                <div v-for="(trending, index) in store.trending.slice(0, 10)" :key="index">
+                    <div class="d-flex">
+                        <h2 class="big-number">
+                            {{ index + 1 }}
+                        </h2>
+                        <CardComponent :id="trending.id" :image="trending.image" />
+                    </div>
+                </div>
             </div>
             <h4 class="text-white p-3 "> Tv Series </h4>
             <div class="row" id="card-container">
@@ -33,4 +47,36 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#card-container-top-10 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 4rem;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+
+    .card {
+        width: 200px;
+        height: 300px;
+        margin-top: 1rem;
+
+        img {
+            width: 150px;
+            height: 200px;
+            object-fit: cover;
+        }
+    }
+
+    .big-number {
+        font-size: 320px;
+        margin-right: 20px;
+        color: black;
+        font-weight: 900;
+        text-shadow: #6a6a6a 2px 10px 20px;
+        padding: -10px;
+        margin-right: -50px;
+
+    }
+}
+</style>
